9b030e5b222292e8f7971ee73fe24b27f7e35cf0
Tim Smith <tsmith@chef.io>
2018-05-26 16:59:41 -0700

    Add issues_url and source_url metadata
    This adds a link to the repo on Supermarket
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'MIT'
 description      'Installs Ruby from brightbox/ruby-ng PPA'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
+source_url       'https://github.com/3ofcoins/chef-cookbook-ruby-ng'
+issues_url       'https://github.com/3ofcoins/chef-cookbook-ruby-ng/issues'
 
 supports 'ubuntu'
 
